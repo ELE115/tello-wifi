35cfb86670281557fd2e7c136f563c250cc5b161
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-01-21 18:43:58 -0500

    use shell

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+#!/usr/bin/node
+
 const util = require('util');
 const dgram = require('dgram');
 const udp = dgram.createSocket('udp4');
-const Wifi = require('wpa_supplicant');
-// workaround
-const Network = Wifi.prototype._getNetwork.call({ networks: [], }, undefined).constructor;
 
 function promisifier(obj, props) {
   props.forEach((v) => {
@@ -11,87 +10,69 @@ function promisifier(obj, props) {
   });
 }
 
-function ensure(f) {
-  function recursion() {
-    return new Promise((resolve, reject) => {
+function ensure(f, to) {
+  return new Promise((resolve, reject) => {
+    function recursion() {
       try {
         if (f())
           return resolve();
-        setTimeout(recursion, 500);
+        if (to === undefined || to-- > 0)
+          setTimeout(recursion, 100);
+        else
+          reject({ timeout: true });
       } catch (e) {
         reject(e);
       }
-    });
-  }
+    }
+    recursion();
+  });
 }
 
 promisifier(udp, ['send', 'bind']);
-promisifier(Wifi.prototype, ['clear', 'scan']);
-promisifier(Network.prototype, ['add', 'select', 'remove', 'connect', 'disconnect']);
 
-const wifi = new Wifi();
 const queue = [];
-
-wifi.on('update', () => {
-  const cur = wifi.currentNetwork;
-  if (cur) {
-    console.log(`Connected to ${cur.ssid}`);
-  } else {
-    console.log('Connected to nothing');
-  }
-});
 
 async function prepare() {
   udp.on('message', (msg, rinfo) => {
-    console.log(`${rinfo.address}:${rinfo.port} -> here: ${msg}`);
+    console.log(`${rinfo.address}:${rinfo.port} -> here: \x1b[1;33m${msg}\x1b[0m`);
     queue.push(msg);
   });
+  console.log('binding');
   await udp.bind(0, undefined);
 }
 
-async function get() {
-  await ensure(() => queue.length);
+async function get(to) {
+  try {
+    await ensure(() => queue.length, to);
+  } catch (e) {
+    if (e.timeout)
+      return undefined;
+    throw e;
+  }
   return queue.shift();
 }
 
-async function command(msg) {
-  await udp.send(Buffer.from(msg), 8889, '192.168.10.1');
-  await get();
+async function command(msg, match, to) {
+  while (to === undefined || to-- >= 0) {
+    console.log(`here -> 192.168.10.1:8889: \x1b[1;32m${msg}\x1b[0m`);
+    await udp.send(Buffer.from(msg), 8889, '192.168.10.1');
+    const res = await get(25);
+    if (res && res.toString('utf-8') === match)
+      return;
+  }
+  console.error('Timeout');
+  proces.exit(1);
 }
 
 async function fix() {
-  await command('command');
-  await command(`ap ${ssid} ${pass}`);
+  await command('command', 'ok');
+  await command(`ap ${process.argv[2]} ${process.argv[3]}`, 'OK,drone will reboot in 3s');
+  await new Promise((resolve) => { setTimeout(resolve, 500); });
+  console.log('Finished!');
+  process.exit(0);
 }
 
-async function scan() {
-  await wifi.scan();
-  await ensure(() => !wifi.scanning);
-  const reg = /^Tello-[A-Z0-9]{6}$/;
-  return wifi.networks.filter((w) => w.ssid.match(reg));
-}
-
-async function run() {
-  await prepare();
-  while (true) {
-    const res = await scan();
-    if (res.length) {
-      for (let net of res) {
-        console.log(`Found ${net.ssid},${net.frequency},${net.signal}`);
-      }
-      for (let net of res) {
-        console.log(`Connecting to ${net.ssid}`);
-        await net.connect();
-        await ensure(() => wifi.currentNetwork === net);
-        await net.disconnect();
-      };
-      await ensure(() => !wifi.currentNetwork);
-      await new Promise((r) => { setTimeout(r, 15000); });
-    } else {
-      console.log('Found nothing');
-      await new Promise((r) => { setTimeout(r, 5000); });
-    }
-  }
-}
-
-wifi.on('ready', run);
+prepare().then(fix).catch((err) => {
+  console.error(err);
+  process.exit(114);
+});
